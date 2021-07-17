7f039fc44dca84f39db8e485b63c5d93bd63cb57
Mateusz Baran <mateuszbaran89@gmail.com>
2021-07-17 16:08:32 +0200

    initial version of Lie-Euler

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,20 @@
 using Test
 using ManifoldDiffEq
+using Manifolds
+using OrdinaryDiffEq
+using LinearAlgebra
+
 @testset "ManifoldDiffEq" begin
-    @test true
+
+    A = ManifoldDiffEq.ManifoldDiffEqOperator{Float64}() do u, p, t
+        return cross(u, [1.0, 0.0, 0.0])
+    end
+    prob = ODEProblem(A, [0.0, 1.0, 0.0], (0, 2.0))
+    sol1 = solve(
+        prob,
+        ManifoldDiffEq.ManifoldLieEuler(Sphere(2), ExponentialRetraction()),
+        dt = 1 / 8,
+    )
+
+    @test sol1(0.0) ≈ [0.0, 1.0, 0.0]
 end
