72cce98c1c6f03a898a015f8e51dedfa7ff87feb
Mateusz Baran <mateuszbaran89@gmail.com>
2021-07-22 19:27:25 +0200

    formatting

--- src/solvers.jl
+++ src/solvers.jl
@@ -1,6 +1,7 @@
 
 
-struct ManifoldInterpolationData{F,uType,tType,kType,cacheType,TM} <: OrdinaryDiffEq.OrdinaryDiffEqInterpolation{cacheType}
+struct ManifoldInterpolationData{F,uType,tType,kType,cacheType,TM} <:
+       OrdinaryDiffEq.OrdinaryDiffEqInterpolation{cacheType}
     f::F
     timeseries::uType
     ts::tType
@@ -14,37 +15,68 @@ function (interp::ManifoldInterpolationData)(t, idxs, deriv, p, continuity)
     return ode_interpolation(t, interp, idxs, deriv, p, continuity)
 end
 
-function manifold_linear_interpolation(M::AbstractManifold, Θ, dt, p, q, idxs::Nothing,T::Type{Val{0}})
+function manifold_linear_interpolation(
+    M::AbstractManifold,
+    Θ,
+    dt,
+    p,
+    q,
+    idxs::Nothing,
+    T::Type{Val{0}},
+)
     return shortest_geodesic(M, p, q, Θ)
 end
 
-function manifold_linear_interpolation(M::AbstractManifold, Θ, dt, ps, qs, idxs, T::Type{Val{0}})
+function manifold_linear_interpolation(
+    M::AbstractManifold,
+    Θ,
+    dt,
+    ps,
+    qs,
+    idxs,
+    T::Type{Val{0}},
+)
     return map(i -> shortest_geodesic(M, ps[i], qs[i], Θ), idxs)
 end
 
-function ode_interpolation(tval::Number, id::ManifoldInterpolationData, idxs, deriv, p, continuity::Symbol=:left)
+function ode_interpolation(
+    tval::Number,
+    id::ManifoldInterpolationData,
+    idxs,
+    deriv,
+    p,
+    continuity::Symbol = :left,
+)
     # implmenented based on `ode_interpolation` from OrdinaryDiffEq.jl
-    @unpack ts,timeseries,ks,f,cache = id
-    @inbounds tdir = sign(ts[end]-ts[1])
+    @unpack ts, timeseries, ks, f, cache = id
+    @inbounds tdir = sign(ts[end] - ts[1])
 
     if continuity === :left
         # we have i₋ = i₊ = 1 if tval = ts[1], i₊ = i₋ + 1 = lastindex(ts) if tval > ts[end],
         # and otherwise i₋ and i₊ satisfy ts[i₋] < tval ≤ ts[i₊]
-        i₊ = min(lastindex(ts), OrdinaryDiffEq._searchsortedfirst(ts,tval,2,tdir > 0))
+        i₊ = min(lastindex(ts), OrdinaryDiffEq._searchsortedfirst(ts, tval, 2, tdir > 0))
         i₋ = i₊ > 1 ? i₊ - 1 : i₊
     else
         # we have i₋ = i₊ - 1 = 1 if tval < ts[1], i₊ = i₋ = lastindex(ts) if tval = ts[end],
         # and otherwise i₋ and i₊ satisfy ts[i₋] ≤ tval < ts[i₊]
-        i₋ = max(1, OrdinaryDiffEq._searchsortedlast(ts,tval,1,tdir > 0))
+        i₋ = max(1, OrdinaryDiffEq._searchsortedlast(ts, tval, 1, tdir > 0))
         i₊ = i₋ < lastindex(ts) ? i₋ + 1 : i₋
     end
-    
+
     begin
         dt = ts[i₊] - ts[i₋]
-        Θ = iszero(dt) ? oneunit(tval) / oneunit(dt) : (tval-ts[i₋]) / dt
+        Θ = iszero(dt) ? oneunit(tval) / oneunit(dt) : (tval - ts[i₋]) / dt
 
         # TODO: use manifold Hermite interpolation
-        val = manifold_linear_interpolation(id.manifold,Θ,dt,timeseries[i₋],timeseries[i₊],idxs,deriv)
+        val = manifold_linear_interpolation(
+            id.manifold,
+            Θ,
+            dt,
+            timeseries[i₋],
+            timeseries[i₊],
+            idxs,
+            deriv,
+        )
     end
 
     return val
@@ -140,8 +172,16 @@ function build_solution(
     kwargs...,
 )
     T = eltype(eltype(u))
-    
-    manifold_interp = ManifoldInterpolationData(interp.f, interp.timeseries, interp.ts, interp.ks, interp.dense, interp.cache, prob.manifold)
+
+    manifold_interp = ManifoldInterpolationData(
+        interp.f,
+        interp.timeseries,
+        interp.ts,
+        interp.ks,
+        interp.dense,
+        interp.cache,
+        prob.manifold,
+    )
     return ODESolution{
         T,
         1,
